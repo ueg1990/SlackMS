25ac249091b693a82f233e8a0c931d6cedac0764
ueg1990 <uehtesham90@gmail.com>
2015-03-29 00:19:00 -0400

    Retrieve team member user info from text

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
  
 import (
   "encoding/xml"
-  //"encoding/json"
+  "encoding/json"
   "net/http"
   "fmt"
   //"bytes"
   // "net/url"
-  //"io/ioutil"
+  "io/ioutil"
   "strings"
   "os"
 )
@@ -21,60 +21,112 @@ type TwiML struct {
 }
  
 func main() {
-  http.HandleFunc("/", hello)
+  // http.HandleFunc("/", hello)
+  populate_database()
+  fmt.Println("Database populated")
   http.HandleFunc("/twiml", twiml)
   http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-	// data := map[string]interface{}{}
-	// token := "xoxp-2757127568-2813046014-4209073904-35634c"
-	// resp, _ := http.Get("https://slack.com/api/users.list?token="+token)
-	// fmt.Println(resp)
-	// defer resp.Body.Close()
- //    body, _ := ioutil.ReadAll(resp.Body)
- //    json.Unmarshal(body, &data)
- //    fmt.Println(data)
-  
-
- //  defer resp.Body.Close()
- //  body, err := ioutil.ReadAll(resp.Body)
-
- //  if nil != err {
- //    fmt.Println("errorination happened reading the body", err)
- //    return
- //  }
+  //fmt.Println(user_info)
     
+}
+
+type Request struct {
+   Ok bool
+   Members []User
+}
+
+var user_info = make(map[string]map[string]string)
+
+func populate_database() {
+    
+  data := Request{} //map[string]interface{}{} 
+  token := "xoxp-2757127568-2813046014-4209073904-35634c"
+  resp, _ := http.Get("https://slack.com/api/users.list?token="+token)
+  //fmt.Println(resp)
+  defer resp.Body.Close()
+  body, _ := ioutil.ReadAll(resp.Body)
+  json.Unmarshal(body, &data)
+  //fmt.Println(data.Members)
+  for _,j := range data.Members {
+    if j.Profile.Phone != "" {
+      temp := make(map[string]string)
+      temp["slack_id"] = j.Id
+      temp["slack_name"] = j.Name
+      temp["slack_email"] = j.Profile.Email
+      temp["slack_profilepic"] = j.Profile.Image_48
+      user_info[j.Profile.Phone] = temp
+    }
+  }
+  //fmt.Println(user_info)
 }
  
 func twiml(w http.ResponseWriter, r *http.Request) {
   r.ParseForm()
   body := r.PostForm["Body"][0]
-  fmt.Println(body)
-  bodyArray := strings.Fields(body)
-  fmt.Println(bodyArray)
-  slack_name := bodyArray[0]
-  slack_channel := bodyArray[1]
-  slack_msg := strings.Join(bodyArray[2:], " ")
-  //resp, _ := http.Post("https://hooks.slack.com/services/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", "text/json",strings.NewReader("{\"text\":\"Random message from usman using Go\", \"channel\" : \"#general\", \"username\" : \"uehtesham90\", \"icon_url\":\"https://secure.gravatar.com/avatar/595b1952765efa4ff448f55a0e71b49a.jpg?s=72&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F3654%2Fimg%2Favatars%2Fava_0006-72.png\"}"))
-  //resp, _ := http.Post("https://hooks.slack.com/services/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", "text/json",strings.NewReader("{\"text\": \""+slack_msg+"\", \"channel\" : \""+channel+"\", \"username\" : \"uehtesham90\", \"icon_url\":\"https://secure.gravatar.com/avatar/595b1952765efa4ff448f55a0e71b49a.jpg?s=72&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F3654%2Fimg%2Favatars%2Fava_0006-72.png\"}"))
-  resp, _ := http.Post("https://hooks.slack.com/services/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", "text/json",strings.NewReader("{\"text\": \""+slack_msg+"\", \"channel\" : \""+slack_channel+"\", \"username\" : \""+slack_name+"\"}"))
-  fmt.Println(resp.Status)
-  msg := "Responding..."
-  if( resp.StatusCode >= 200 && resp.StatusCode < 300 ) {
-  	msg = "Message successfully sent!!!"
-  
-} else {
-  msg = "Message NOT sent!!!"
-}
-  twiml := TwiML{Message: msg}
-  x, err := xml.Marshal(twiml)
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
+  from := r.PostForm["From"][0]
+  if from[:2] == "+1" {
+    from = from[2:]
   }
- 
-  w.Header().Set("Content-Type", "application/xml")
-  w.Write(x)
+  if val, ok := user_info[from]; ok {
+    //fmt.Println("Number in db", val)
+    //fmt.Println(user_info[from])
+    bodyArray := strings.Fields(body)
+    slack_channel := bodyArray[0]
+    //fmt.Println(bodyArray)
+    slack_name := val["slack_name"]
+    slack_profilepic := val["slack_profilepic"]
+    slack_msg := strings.Join(bodyArray[1:], " ")
+    //resp, _ := http.Post("https://hooks.slack.com/services/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", "text/json",strings.NewReader("{\"text\": \""+slack_msg+"\", \"channel\" : \""+channel+"\", \"username\" : \"uehtesham90\", \"icon_url\":\"https://secure.gravatar.com/avatar/595b1952765efa4ff448f55a0e71b49a.jpg?s=72&d=https%3A%2F%2Fslack.global.ssl.fastly.net%2F3654%2Fimg%2Favatars%2Fava_0006-72.png\"}"))
+    resp, _ := http.Post("https://hooks.slack.com/services/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", "text/json",strings.NewReader("{\"text\": \""+slack_msg+"\", \"channel\" : \""+slack_channel+"\", \"username\" : \""+slack_name+"\", \"icon_url\":\""+slack_profilepic+"\"}"))
+    fmt.Println(resp.Status)
+    msg := "Responding..."
+    if( resp.StatusCode >= 200 && resp.StatusCode < 300 ) {
+     msg = "Message successfully sent!!!"
+    
+    } else {
+      msg = "Message NOT sent!!!"
+    }
+    twiml := TwiML{Message: msg}
+    x, err := xml.Marshal(twiml)
+    if err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
+    }
+    w.Header().Set("Content-Type", "application/xml")
+    w.Write(x)
+  } else {
+    fmt.Println("Unrecognized number", from)
+  }
+  
 }
 
 func hello(res http.ResponseWriter, req *http.Request) {
   fmt.Println("Welcome to SlackMS!!!")
+}
+
+type User struct {
+  Id                string
+  Name              string
+  Deleted           bool
+  Color             string
+  Profile           profile
+  Is_Admin          bool
+  Is_Owner          bool
+  Has_2fa          bool
+  Has_Files         bool
+  
+}
+
+type profile struct {
+  First_Name   string
+  Last_Name    string
+  Real_Name    string
+  Email        string
+  Skype        string
+  Phone        string
+  Image_24     string
+  Image_32     string
+  Image_48     string
+  Image_72     string
+  Image_192    string 
 }
